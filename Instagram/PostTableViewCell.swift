a0fc58894f8175b2df3884a2455f7851cb7adc10
comu2e <dvlpEiji@gmail.com>
2016-07-03 15:34:23 +0900

    投稿データのクラス作製

---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,55 @@
+//
+//  PostTableViewCell.swift
+//  
+//
+//  Created by Eiji Takahashi on 2016/07/03.
+//
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    var postData: PostData!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    override func layoutSubviews() {
+        
+        postImageView.image = postData.image
+        captionLabel.text = "\(postData.name!) : \(postData.caption!)"
+        
+        let likeNumber = postData.likes.count
+        likeLabel.text = "\(likeNumber)"
+        
+        let formatter = NSDateFormatter()
+        formatter.locale = NSLocale(localeIdentifier: "ja_JP")
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.stringFromDate(postData.date!)
+        dateLabel.text = dateString
+        
+        if postData.isLiked {
+            let buttonImage = UIImage(named: "like_exist")
+            likeButton.setImage(buttonImage, forState: UIControlState.Normal)
+        } else {
+            let buttonImage = UIImage(named: "like_none")
+            likeButton.setImage(buttonImage, forState: UIControlState.Normal)
+        }
+        
+        super.layoutSubviews()
+    }
+}
+}
